6d48ad61372e3b67be198d316651b067a33dc843
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-11-05 21:22:58 -0800

    responsive test

--- frontend/components/TestimonialCard.jsx
+++ frontend/components/TestimonialCard.jsx
@@ -27,7 +27,7 @@ const TestimonialCard = ({ testimony, onTestimonialDelete }) => {
 
   return (
     <div className={`z-5 bg-black bg-opacity-30 text-didactGothic flex flex-col justify-center items-center gap-2 p-6 mx-auto w-full md:max-w-xl border-stone-300 rounded-md shadow-lg text-m transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110`}>
-      <div className="text-lightcream text-center">"{testimony.content}"</div>
+      <div className="text-lightcream sm:text-xl lg:text-2xl text-center">"{testimony.content}"</div>
       <div className="text-center my-10">
         <div className="text-rose-200 text-xl text-center">{testimony.firstName}</div>
         <div className="text-rose-200 text-xl text-center">{testimony.lastName}</div>
